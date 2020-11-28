42e7e2cd608f7f1d78c0165db889c9c9d75f43a0
melissaruiz1 <melissaruiz913@yahoo.com>
2020-11-27 16:54:46 -0800

    fixes app crash with the bottom nav

--- app/src/main/java/com/bignerdranch/android/bookz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/bookz/MainActivity.kt
@@ -13,20 +13,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 
-private var mDatabaseReference: DatabaseReference? = null
-private var mDatabase: FirebaseDatabase? = null
-private var mAuth: FirebaseAuth? = null
-//UI elements
-private var tvFirstName: TextView? = null
-private var tvLastName: TextView? = null
-private var tvEmail: TextView? = null
-private var tvEmailVerifiied: TextView? = null
-private var tvSchool: TextView? = null
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //initialise()
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
@@ -38,30 +28,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-    }
-    /*private fun initialise() {
-        mDatabase = FirebaseDatabase.getInstance()
-        mDatabaseReference = mDatabase!!.reference!!.child("Users")
-        mAuth = FirebaseAuth.getInstance()
-        tvFirstName = findViewById<View>(R.id.tv_first_name) as TextView
-        tvLastName = findViewById<View>(R.id.tv_last_name) as TextView
-        tvSchool = findViewById<View>(R.id.tv_school_name) as TextView
-        tvEmail = findViewById<View>(R.id.tv_email) as TextView
-        tvEmailVerifiied = findViewById<View>(R.id.tv_email_verifiied) as TextView
-    } */
-    override fun onStart() {
-        super.onStart()
-        val mUser: FirebaseUser? = mAuth!!.currentUser
-        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
-        tvEmail!!.text = mUser.email
-        tvEmailVerifiied!!.text = mUser.isEmailVerified.toString()
-        mUserReference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                tvFirstName!!.text = snapshot.child("firstName").value as String
-                tvLastName!!.text = snapshot.child("lastName").value as String
-                tvSchool!!.text = snapshot.child("schoolName").value as String
-            }
-            override fun onCancelled(databaseError: DatabaseError) {}
-        })
     }
 }
