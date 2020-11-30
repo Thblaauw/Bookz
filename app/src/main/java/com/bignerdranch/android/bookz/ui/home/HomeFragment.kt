585cf6e7a9779d1d39fb08004be97cdfe56684e4
thblaauw <thblaauw@gmail.com>
2020-11-30 14:04:21 -0800

    Sorta working - Stable

--- app/src/main/java/com/bignerdranch/android/bookz/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/bookz/ui/home/HomeFragment.kt
@@ -8,24 +8,26 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bignerdranch.android.bookz.BookFragment
+import com.bignerdranch.android.bookz.BookListFragment
 import com.bignerdranch.android.bookz.R
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val fragment = BookListFragment.newInstance()
+        childFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+
         return root
     }
 }
