8b08445720727e573d01035a449ca0acc0f13672
melissaruiz1 <melissaruiz913@yahoo.com>
2020-12-01 01:26:36 -0800

    Bug Fixes

--- app/src/main/java/com/bignerdranch/android/bookz/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/bookz/ui/home/HomeFragment.kt
@@ -14,7 +14,7 @@ import com.bignerdranch.android.bookz.BookListFragment
 import com.bignerdranch.android.bookz.R
 import java.util.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), BookListFragment.Callbacks {
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -26,10 +26,20 @@ class HomeFragment : Fragment() {
         val fragment = BookListFragment.newInstance()
         childFragmentManager
             .beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
+
+        return root
+    }
+
+    override fun onBookSelected(bookId: UUID) {
+        Log.d("F1", "On Home Fragment")
+        val fragment = BookFragment.newInstance(bookId)
+        childFragmentManager
+            .beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .addToBackStack(null)
             .commit()
 
-        return root
     }
 }
