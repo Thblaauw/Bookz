cee4ec2bb5890c059effccaf36dc758aef163d9a
ToAotao <33048685+ToAotao@users.noreply.github.com>
2020-12-05 18:38:09 -0800

    Update BookListViewModel.kt

--- app/src/main/java/com/bignerdranch/android/bookz/BookListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/bookz/BookListViewModel.kt
@@ -20,41 +20,14 @@ class BookListViewModel : ViewModel() {
 
         return mutableData
     }
-    /*val books = mutableListOf<Book>()
+    fun searchBooksByTitle(title: String): LiveData<MutableList<Post>>{
+        val mutableData = MutableLiveData<MutableList<Post>>()
+        repo.searchByTitle(title).observeForever{postList->
+            mutableData.value = postList
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
-
-class BookListViewModel : ViewModel() {
-    val books = mutableListOf<Book>()
-
-
-    private val mutableSearchTerm = MutableLiveData<String>()
-    init {
-        for (i in 0 until 100) {
-            val book = Book()
-            book.title = "Book #$i"
-            book.author = "Author #$i"
-            book.price = "$10.00 or trade"
-            book.ISBN = "90883019283"
-            book.description = "Cool Description #$i"
-            book.condition = i % 2 == 0
-            books += book
         }
 
-        mutableSearchTerm.value = "Physics"
-
-        /*
-        Transformations.switchMap(mutableSearchTerm) {
-                searchTerm -> flickrFetchr.searchPhotos(searchTerm)
-        }
-        */
-
+        return mutableData
     }
-
-
-
-     */
 
 }
