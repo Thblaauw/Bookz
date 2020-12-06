07ff87309b129fc713e5a5fbdeb95e1c6b9a7fec
ToAotao <33048685+ToAotao@users.noreply.github.com>
2020-12-05 18:40:28 -0800

    Update BookListFragment.kt

--- app/src/main/java/com/bignerdranch/android/bookz/BookListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/bookz/BookListFragment.kt
@@ -111,24 +111,7 @@ class BookListFragment : Fragment() {
 
 
     }
-/*
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_book_list, container, false)
-        bookRecyclerView = view.findViewById(R.id.book_recycler_view) as RecyclerView
-        bookRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
-
-        return view
-    }
-
-
- */
     override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -161,7 +144,13 @@ class BookListFragment : Fragment() {
 
         })
     }
+    fun filteredData(title: String){
+        viewModel.searchBooksByTitle(title)?.observe(this, Observer {
+            adapter.setListData(it)
+            adapter.notifyDataSetChanged()
 
+        })
+    }
 
 
 
