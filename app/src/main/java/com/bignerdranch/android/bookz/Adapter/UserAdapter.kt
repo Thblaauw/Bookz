1e10d5ac8f8d59866bdbf7c43250ee71e85e108c
melissaruiz1 <melissaruiz913@yahoo.com>
2020-12-06 23:39:55 -0800

    Adds messages fragment with most recent messages and profile pics

--- app/src/main/java/com/bignerdranch/android/bookz/Adapter/UserAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/bookz/Adapter/UserAdapter.kt
@@ -10,9 +10,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.bookz.MessageChatActivity
+import com.bignerdranch.android.bookz.ModelClasses.Chat
 import com.bignerdranch.android.bookz.ModelClasses.Users
 import com.bignerdranch.android.bookz.R
 import com.bignerdranch.android.bookz.R.id.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -25,6 +31,7 @@ class UserAdapter(
     private val mContext: Context
     private val mUsers: List<Users>
     private var isChatCheck: Boolean
+    var lastMsg: String = ""
 
     init {
         this.mUsers = mUsers
@@ -40,12 +47,19 @@ class UserAdapter(
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
         val user: Users = mUsers[i]
         holder.userNameText.text = user!!.getFirstName() + " " + user!!.getLastName()
-        //Picasso.get().load(user.getProfilePicture()).placeholder(R.drawable.ic_baseline_account_circle_24).into(holder.profileImageView)
+        Picasso.get().load(user.getProfilePicture()).into(holder.profileImageView)
+
+        if(isChatCheck) {
+            getLastMessage(user.getUID(),holder.lastMessageText)
+        }
+        else {
+            holder.lastMessageText.visibility = View.GONE
+        }
+        
 
         holder.itemView.setOnClickListener {
             val options = arrayOf<CharSequence> (
-                "Send Message",
-                "Visit Profile"
+                "Send Message"
             )
             val builder: AlertDialog.Builder = AlertDialog.Builder(mContext)
             builder.setItems(options, DialogInterface.OnClickListener { dialog, which ->
@@ -63,6 +77,7 @@ class UserAdapter(
         }
     }
 
+
     override fun getItemCount(): Int {
         return mUsers.size
     }
@@ -77,6 +92,37 @@ class UserAdapter(
             profileImageView = itemView.findViewById(R.id.profile_pic_bar)
             lastMessageText = itemView.findViewById(R.id.message_last)
         }
+
+    }
+
+    private fun getLastMessage(chatUserId: String?, lastMessageText: TextView) {
+        lastMsg = "defaultMsg"
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        val reference = FirebaseDatabase.getInstance().reference.child("Chats")
+
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                for(dataSnapshot in p0.children) {
+                    val chat: Chat? = dataSnapshot.getValue(Chat::class.java)
+                    if(firebaseUser!=null && chat!=null) {
+                        if(chat.getReceiver() == firebaseUser!!.uid && chat.getSender() == chatUserId || chat.getReceiver() == chatUserId && chat.getSender() == firebaseUser!!.uid ) {
+                            lastMsg = chat.getMessage()!!
+                        }
+                    }
+                }
+
+                when(lastMsg) {
+                    "defaultMsg" -> lastMessageText.text = "No Message"
+                    "Attatchment: Image" -> lastMessageText.text = "Attatchment: Image"
+                    else -> lastMessageText.text = lastMsg
+                }
+                lastMsg = "defaultMsg"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
 
     }
 
